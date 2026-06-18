b977bacd19c297ced993d96451ced09b5d9d6983
sankalpdevtester <ohofufic0@gmail.com>
2026-06-18 17:56:11 +0530

    feat: add NFT minting contract logic

--- contracts/NFTMinting.sol
+++ contracts/NFTMinting.sol
@@ -1,28 +1,29 @@
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC721/SafeERC721.sol";
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.5.0/contracts/token/ERC721/ERC721.sol';
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.5.0/contracts/access/Ownable.sol';
 
-contract NFTMinting {
+contract NFTMinting is ERC721, Ownable {
   mapping(address => mapping(uint256 => NFT)) public nftOwners;
-  mapping(uint256 => NFT) public nftList;
 
   struct NFT {
-    uint256 id;
+    uint256 tokenId;
     string name;
     string description;
     string image;
   }
 
-  event NFTMinted(address owner, uint256 id, string name, string description, string image);
+  event NFTMinted(uint256 tokenId, string name, string description, string image);
 
-  function mintNFT(address owner, string memory name, string memory description, string memory image) public {
-    uint256 id = nftList.length++;
-    nftList[id] = NFT(id, name, description, image);
-    nftOwners[owner][id] = nftList[id];
-    emit NFTMinted(owner, id, name, description, image);
+  function mintNFT(string memory _name, string memory _description, string memory _image) public onlyOwner {
+    uint256 tokenId = uint256(keccak256(abi.encodePacked(_name, _description, _image)));
+    NFT memory nft = NFT(tokenId, _name, _description, _image);
+    nftOwners[msg.sender][tokenId] = nft;
+    _mint(msg.sender, tokenId);
+    emit NFTMinted(tokenId, _name, _description, _image);
   }
 
-  function getNFT(uint256 id) public view returns (NFT memory) {
-    return nftList[id];
+  function getNFT(uint256 _tokenId) public view returns (NFT memory) {
+    return nftOwners[msg.sender][_tokenId];
   }
 }
